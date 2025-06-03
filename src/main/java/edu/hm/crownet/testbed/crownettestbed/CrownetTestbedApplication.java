e38121c8d446acbe483775898a4b66bf4fc0934b
vincenzo.auricchio <vincenzo.auricchio@check24.de>
2025-06-03 02:02:05 +0200

    Set up initial project scaffolding using Gradle and Spring Boot

---
+++ src/main/java/edu/hm/crownet/testbed/crownettestbed/CrownetTestbedApplication.java
@@ -0,0 +1,13 @@
+package edu.hm.crownet.testbed.crownettestbed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrownetTestbedApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CrownetTestbedApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/hm/crownet/testbed/crownettestbed/CrownetTestbedApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hm.crownet.testbed.crownettestbed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrownetTestbedApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
